afa2911d6bb54aa072754b2b8a068521b8596b68
jbirkj <jesperbirkp@gmail.com>
2020-02-17 22:55:33 +0100

    added files first time

---
+++ app.py
@@ -0,0 +1,50 @@
+#app.py
+import argparse
+import RPi.GPIO as GPIO
+import bme280
+import time
+from fan import Fan
+from os import system
+
+T = time.time()
+f = Fan()
+
+parser = argparse.ArgumentParser(description='add measure cycle in seconds')
+parser.add_argument("--t", default=10, type=int, help="set the time in seconds - default 10")
+parser.add_argument("--rH", default=60, type=int, help="relative humidity[rH] level for starting fan")
+args=parser.parse_args()
+
+Tcycle = args.t
+rHlimit = args.rH
+
+try:
+    while(True):
+        
+        if (time.time()-T) >= Tcycle:     #T in seconds
+            
+            T = time.time()
+            t,p,rH = bme280.readBME280All()
+            system('clear')
+            print("aflæsning: {0:3.2f}gC, {1:4.0f}hPa, {2:2.1f}rH   ".format(t, p, rH))
+            
+            if rH > rHlimit:
+                f.FanOn()
+                print("Fan on")
+            else:
+                f.FanOff()
+                print("Fan Off")
+                
+except KeyboardInterrupt:
+    print("KB interrupt!")
+    
+except BaseException as e:
+    print("Other exception occurred")
+    print(e.message)
+    
+finally:
+    GPIO.cleanup()
+
+            
+
+
+
