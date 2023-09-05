635fa8c2010f3a274494eb5e224636c350dbaa34
jesperbirkp@gmail.com <jesperbirkp@gmail.com>
2023-09-05 23:38:49 +0200

    app.py updated with Thingspeak connectivity

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests
 from time import time, ctime
 from fan import Fan
 from os import system
-from ubi import UBI_url, TS_url
+from ubi import UBI_url, TS_url, IFTTT_ON_url, IFTTT_OFF_url
 
 T = time()
 f = Fan()
@@ -18,21 +18,34 @@ args=parser.parse_args()
 
 Tcycle = args.t
 rHlimit = args.rH
+
+url = TS_url
+#url = UBI_url
+
 try:
     #entry state to do measure at start
     t,p,rH = bme280.readBME280All()
     print("Reading @ {3}: {0:3.2f}gC, {1:4.0f}hPa, {2:2.1f}rH   ".format(t, p, rH, ctime())) 
     try:
-        r = requests.post(UBI_url, {'Temperature': t, 'Pressure': p, 'Humidity':rH} )
+        #r = requests.post(UBI_url, {'Temperature': t, 'Pressure': p, 'Humidity':rH} )
+        r = requests.post(TS_url, {'field1': t, 'field2': p, 'field3':rH} )
     except:
-        print("some error uccurred during POST data to ubidots")
+        print("Initial run - some error uccurred during POST data to ubidots" + str(r.status_code) + r.text )
         
     if rH > rHlimit:
         f.FanOn()
         print("Fan on")
+        try:
+            r = requests.post( IFTTT_ON_url, {'value1': '1', 'value2': '2', 'value3': '3'} )
+        except:
+            print("IFTTT_ON failed " + str(r.status_code) + r.text)
     else:
         f.FanOff()
         print("Fan Off")
+        try:
+            r = requests.post( IFTTT_OFF_url, {'T': t, 'P': p, 'H': rH} )
+        except:
+            print("IFTTT_OFF failed" + r.text)
 
     #entering endless loop for continous runtime
     while(True):
@@ -44,9 +57,10 @@ try:
             #print("Reading {3}: {0:3.2f}gC, {1:4.0f}hPa, {2:2.1f}rH   ".format(t, p, rH, round(T)))
             print("Reading @ {3}: {0:3.2f}gC, {1:4.0f}hPa, {2:2.1f}rH   ".format(t, p, rH, ctime())) 
             try:
-                r = requests.post(UBI_url, {'Temperature': t, 'Pressure': p, 'Humidity':rH} )
+                #r = requests.post(UBI_url, {'Temperature': t, 'Pressure': p, 'Humidity':rH} )
+                r = requests.post(TS_url, {'field1': t, 'field2': p, 'field3':rH} )
             except:
-                print("some error eccurred during data POST to UBIduts")
+                print("Loop run -some error eccurred during data POST. #" + str(r.status_code) + r.text )
             
             if rH > rHlimit:
                 f.FanOn()
